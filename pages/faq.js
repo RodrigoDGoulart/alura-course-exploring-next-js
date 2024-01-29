c8a83372847067059b3d53c503f9f7a42491ebf5
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-01-29 12:42:34 -0300

    feat: implement getStaticProps on faq screen

--- pages/faq.js
+++ pages/faq.js
@@ -1,10 +1,33 @@
 import Link from "../src/components/Link";
 
-export default function Faq() {
-    return(
-        <div> 
-            <h1>FAQ</h1>
-            <Link href='/'>Home</Link>
-        </div>
-    )
+export async function getStaticProps() {
+  const API_URL =
+    "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
+  const faq = await fetch(API_URL)
+    .then((res) => res.json())
+    .then((res) => res);
+  return {
+    props: {
+      faq,
+    },
+  };
+}
+
+export default function Faq({ faq }) {
+  return (
+    <div>
+      <h1>FAQ</h1>
+      <Link href="/">Home</Link>
+      <ul>
+        {faq.map(({ answer, question }) => (
+          <li key={question}>
+            <article>
+              <h1>{question}</h1>
+              <p>{answer}</p>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
