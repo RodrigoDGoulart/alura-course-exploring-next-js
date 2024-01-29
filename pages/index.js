c4f0bb58c863fa8ce06c1a4a6f02898ada897a77
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-01-29 12:04:25 -0300

    feat: components and styles

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
-import Link from "../src/components/Link";
-import Title from "../src/components/Title";
+import HomeScreen from "../src/screens/HomeScreen";
 
-export default function HomePage() {
-  return (
-    <div>
-      <Title as="h2">Alura cases - Home</Title>
-      <Link href="/faq">FAQ</Link>
-    </div>
-  );
-}
+export default HomeScreen;
+
+// import Link from "../src/components/Link";
+// import Title from "../src/components/Title";
+// import Footer from "../src/components/patterns/Footer";
+
+// export default function HomePage() {
+//   return (
+//     <div>
+//       <Title as="h2">Alura cases - Home</Title>
+//       <Link href="/faq">FAQ</Link>
+//       <Footer />
+//     </div>
+//   );
+// }
+
