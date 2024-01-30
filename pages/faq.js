b8af2c92bd8a7285ddd5b92bc656b9a277824380
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-01-30 09:51:27 -0300

    docs: getStaticProps & getServerSideProps comments

--- pages/faq.js
+++ pages/faq.js
@@ -1,6 +1,14 @@
 import Link from "../src/components/Link";
 
-export async function getStaticProps() {
+// obs.: ambas as funções getStaticProps e getServerSideProps rodam a cada acesso quando projeto está rodando como dev
+
+// export async function getStaticProps() {
+// getStaticProps roda uma vez na build. Não é rodado outra vez no acesso
+
+export async function getServerSideProps() {
+// getServerSideProps é rodado a cada acesso na página
+
+  console.log('rodando');
   const API_URL =
     "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
   const faq = await fetch(API_URL)
