9f47be1b55d3495b1a9cbd9846954eb0fe7a3835
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-01-30 10:17:03 -0300

    doc: google analytics import comments

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
+import Head from "next/head";
 import GlobalStyle from "../src/theme/globalStyle";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        {/* importando script do google analytics */}
+        {/* <script async src='https://www,googletagmanager.com/gatg/qweqweqwe'></script>
+        <script 
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = ....
+              ... restante do código
+            `
+          }}
+        /> */}
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
